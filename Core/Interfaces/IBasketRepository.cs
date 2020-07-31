c7fd2a103f8ad4a9435ba823a1b52816b7c61b89
Faizan Sarwer <fsarwer@symplr.com>
2020-08-01 00:32:59 +0530

    end section 13

---
+++ Core/Interfaces/IBasketRepository.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string id);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+
+        Task<bool> DeleteBasketAsync(string id);
+    }
+}
